a2723de479839512565e8df9779458106ae19277
Daiane Sousa <daisousa.c@gmail.com>
2021-08-23 20:22:43 -0300

    change name

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -8,7 +8,7 @@ export function SignInButton() {
   return isUserLoggedIn ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      Diego Fernandes
+      Daiane Sousa
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
