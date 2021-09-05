dbcf1de724d8376c0e941070ccdb9a5b5e734739
Daiane Sousa <daisousa.c@gmail.com>
2021-09-05 16:00:17 -0300

    add page post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,6 +4,7 @@ import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom'
 import styles from './styles.module.scss';
+import Link from 'next/link';
 
 type Post = {
   slug: string;
@@ -16,7 +17,7 @@ interface PostsProps {
   posts: Post[];
 }
 
-export default function Posts({ posts }:PostsProps) {
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -25,14 +26,16 @@ export default function Posts({ posts }:PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <a href="#" key={post.slug}>
-              <time>
-                {post.updatedAt}
-              </time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
-          )) }
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
+              <a>
+                <time>
+                  {post.updatedAt}
+                </time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
+          ))}
 
         </div>
       </main>
